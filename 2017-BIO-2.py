5729205cb61263f8fc57fc6e2cd9dbddb7ef5e37
WebCoder49 <69071853+WebCoder49@users.noreply.github.com>
2021-09-26 08:56:44 +0100

    Hour of work on 2017-Q2

--- 2017-BIO-2.py
+++ 2017-BIO-2.py
@@ -1,23 +1,152 @@
 # Dots and Boxes
 
-grid_width = 6
-grid_height = 6
-grid_size = grid_height * grid_width
+class Grid:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.size = width * height
 
-# Initialise grid as array
+        # Create arrays for vertical and horizontal lines
+        vertical_lines = []
+        for x in range(width):
+            column = []
+            for y in range(height-1):
+                column.append(False)
+            vertical_lines.append(column)
 
-vertical_lines = []
-for y in range(5):
-    row=[]
-    for x in range(5):
-        row.append(False)
-    vertical_lines.append(row)
+        horizontal_lines = []
+        for y in range(height):
+            row = []
+            for x in range(width-1):
+                row.append(False)
+            horizontal_lines.append(row)
 
-horizontal_lines = []
-for y in range(5):
-    row=[]
-    for x in range(5):
-        row.append(False)
-    horizontal_lines.append(row)
+        self.horizontal_lines = horizontal_lines
+        self.vertical_lines = vertical_lines
 
-def add_line(dot_number, )
+        # Create arrays for boxes won
+        boxes_won = []
+        for y in range(height-1):
+            row = []
+            for x in range(width-1):
+                row.append(0)
+            boxes_won.append(row)
+        self.boxes_won = boxes_won
+
+    def display_boxes_won(self):
+        chars = ["*","X","O"]
+        for row in self.boxes_won:
+            row_str = ""
+            for col in row:
+                row_str += chars[col]
+            print(row_str)
+
+    def draw_line(self, player, position, direction):
+        # Turn position into x, y
+        x = position % self.width
+        y = position // self.width
+
+        # Get x, y of line
+        if(direction == 0):
+            # Upwards
+            if(y == 0):
+                return False # impossible
+            else:
+                y -= 1
+        elif (direction == 2):
+            # Down
+            if (y == (self.height-1)):
+                return False  # impossible
+            # Don't need to change y
+        elif (direction == 3):
+            # Left
+            if (x == 0):
+                return False  # impossible
+            else:
+                x -= 1
+        elif (direction == 1):
+            # Right
+            if (x == (self.width - 1)):
+                return False  # impossible
+            # Don't need to change x
+
+        if(direction & 1 == 0):
+            # Even - vertical lines
+            if(self.vertical_lines[x][y]): return False # Already taken
+            self.vertical_lines[x][y] = True
+
+            if(x != (self.width - 1) and y != (self.height - 1)):
+                if(self.horizontal_lines[y][x] and self.horizontal_lines[y+1][x] and self.vertical_lines[x+1][y]):
+                    # Box to right
+                    self.boxes_won[y][x] = player
+
+            if(x != 0 and y != (self.height - 1)):
+                if (self.horizontal_lines[y][x-1] and self.horizontal_lines[y + 1][x-1] and self.vertical_lines[x - 1][y]):
+                    # Box to left
+                    self.boxes_won[y][x-1] = player
+        else:
+            # Odd - horizontal lines
+            if (self.horizontal_lines[y][x]): return False  # Already taken
+            self.horizontal_lines[y][x] = True
+
+            if (y != (self.height - 1) and x != (self.width - 1)):
+                if (self.vertical_lines[x][y] and self.vertical_lines[x + 1][y] and self.horizontal_lines[y + 1][x]):
+                    # Box below
+                    self.boxes_won[y][x] = player
+
+            if (y != 0 and x != (self.width - 1)):
+                if (self.horizontal_lines[x][y - 1] and self.horizontal_lines[x + 1][y - 1] and self.horizontal_lines[y - 1][x]):
+                    # Box above
+                    self.boxes_won[y - 1][x] = player
+
+        return True
+
+class Player:
+    def __init__(self, id, position, modifier, anticlockwise):
+        self.position = position - 1 # zero-indexed
+        self.modifier = modifier
+        self.anticlockwise = anticlockwise
+        self.id = id
+
+    def turn(self, grid):
+        # Increase pos by modifier
+        self.position += self.modifier
+        self.position = self.position % grid.size
+
+        for i in range(grid.size): # Keep trying for every position in grid if not successful
+            for direction in range(4):
+                if(grid.draw_line(self.id, self.position, direction)):
+                    return  # Successful
+
+            # Not successful - increment
+            self.position += 1
+            self.position = self.position % grid.size
+
+class Game:
+    def __init__(self, grid, turns, *players): # Player classes w/ pos and mod
+        self.grid = grid
+
+        if(turns > 60):
+            self.turns = 60
+        elif(turns < 1):
+            self.turns = 1
+        else:
+            self.turns = turns
+
+        self.players = players
+        self.player_n = len(players)
+
+    def play(self):
+        for i in range(self.turns):
+            self.players[i % self.player_n].turn(self.grid)
+
+
+grid = Grid(6, 6)
+player_1 = Player(1, int(input("Player 1 Pos: ")), int(input("Player 1 Mod: ")), False)
+player_2 = Player(2, int(input("Player 2 Pos: ")), int(input("Player 2 Mod: ")), True)
+game = Game(grid, int(input("No. of Turns: ")), player_1, player_2)
+
+game.play()
+grid.display_boxes_won()
+
+# Still bugs
